0080e541308effc20d620d87b852dd228580fa29
Miguel Galindo <miguelgalindo1214@gmail.com>
2025-05-07 14:12:33 -0400

    incorporating react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
 import './App.css';
-import Todo from './components/Todo';
-import Title from './components/Title';
-import Modal from './components/Modal';
-import React, { useState } from 'react';
-import Counter from './components/Counter';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home.jsx'
+import Contact from './pages/Contact';
+import Nav from './components/Nav.jsx';
+
 
 function App() {
 
-  const [showModal, setShowModal] = useState(false);
 
   return (
     <div>
-      <Title />
-      <div>
-        <input type="text" onChange={(event) => {
-          console.log(event.target.value)
-        }} />
-        <button onClick={() => setShowModal(true)}> Add todo</button>
-      </div>
-      <Todo title="Finish Frontend Simplified" paragraph="Code along step by step" />
-      <Todo title="Finish Interview Section" paragraph="Finish every interview in the next 2 weeks" />
-      <Todo title="Land $100k Job" paragraph="Apply to 100 jobs" />
-      {showModal && <Modal confirmation ="Are you sure?" /> }
-
-      <Counter />
+      <Router>
+      <Nav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+    return (
+        <div>
+         Contact Page
+        </div>
+    )
+}
+
+
+export default Contact;

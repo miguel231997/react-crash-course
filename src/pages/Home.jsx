72241544fb9737dbd75e39e70bc49fe50b2be47a
Miguel Galindo <miguelgalindo1214@gmail.com>
2025-05-07 14:21:35 -0400

    incorporating axios to fetch API example

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
+import React,  { useEffect, useState } from "react";
+import axios from "axios";
+
 function Home () {
+    const [ users, setUsers] = useState([]);
+
+    async function fetchUsers() {
+        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
+        setUsers(data);
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
     return (
         <div>
-            Home page
+            <h1> { users.length > 0 && users[0].name } </h1>
         </div>
     )
 }
